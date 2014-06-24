5e4a822192468723e5da80895787d9cd07248d06
Michael Marino <mmarino@gmail.com>
2014-06-24 16:45:59 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='pynedm',
-  version='0.0.1.dev1',
+  version='0.0.2.dev1',
   packages=find_packages('src'),
   package_dir = {'':'src'},
   url='https://github.com/nEDM-TUM/Python-Slow-Control',
