81fe22a4514c073c06eb36a2073758c93f0d191a
Michael Marino <mmarino@gmail.com>
2015-05-15 19:52:43 +0200

    export set_account as well.

--- pynedm/__init__.py
+++ pynedm/__init__.py
@@ -1,1 +1,7 @@
-from .utils import wait, stop_listening, listen, should_stop, start_process, write_document_to_db
+from .utils import wait,\
+                   stop_listening,\
+                   listen,\
+                   should_stop,\
+                   start_process,\
+                   write_document_to_db,\
+                   set_account
