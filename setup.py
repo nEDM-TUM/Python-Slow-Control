cd6460bcfdfed809fce7b1de46ddb15f0291c387
Michael Marino <mmarino@gmail.com>
2015-10-29 21:40:23 +0100

    Require 0.5.8 cloudant.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
   name='pynedm',
-  version='0.0.17',
+  version='0.1.0',
   packages=['pynedm'],
   url='https://github.com/nEDM-TUM/Python-Slow-Control',
   author='Michael Marino',
   author_email='mmarino@gmail.com',
-  install_requires=['cloudant', 'pycurl', 'autobahn', 'netifaces', 'twisted']
+  install_requires=['cloudant == 0.5.8', 'pycurl', 'autobahn', 'netifaces', 'twisted']
 )
