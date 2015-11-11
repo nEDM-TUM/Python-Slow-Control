8da80a1ee67a203ef7836c7ce78097acc080f31e
Michael Marino <mmarino@gmail.com>
2015-11-11 18:05:41 +0100

    Should have been 0.5.9

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
   url='https://github.com/nEDM-TUM/Python-Slow-Control',
   author='Michael Marino',
   author_email='mmarino@gmail.com',
-  install_requires=['cloudant == 0.5.8', 'pycurl', 'autobahn', 'netifaces', 'twisted']
+  install_requires=['cloudant == 0.5.9', 'pycurl', 'autobahn', 'netifaces', 'twisted']
 )
