78e6c69102f58a92778846ddf0d98d456492ffe1
Michael Marino <mmarino@gmail.com>
2015-11-12 10:27:13 +0100

    Update requirements and dependencies.

--- setup.py
+++ setup.py
@@ -13,5 +13,9 @@ setup(
     'autobahn',
     'netifaces',
     'twisted'
+  ],
+  dependency_links=[
+    "https://github.com/nEDM-TUM/cloudant-python/tarball/nedm-version-0.5.9#egg=cloudant-0.5.9-nedm"
   ]
+
 )
